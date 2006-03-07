c9b202b91640e2181c0df028bfdb1b76464388b6
kwijesekera <kwijesekera@ddaff062-d30e-0410-924e-b8927a9d2c55>
2006-03-07 21:02:22 +0000

    fixed bugs which caused various functions to not work

--- trunk/mandrigo/inc/sql/mysqli.class.php
+++ trunk/mandrigo/inc/sql/mysqli.class.php
@@ -93,29 +93,9 @@ class db{
     
     //function to find a result in a query
     //returns the result value
-    function db_fetchresult($table,$value,$params,$row=0){
+    function db_fetchresult($table,$fields,$params,$row=0){
 		$tmp_value="";
-		$new_value="";
-		if(ereg(",",$value)){
-			$value=explode($value);
-			for($i=0;$i<count($value);$i++){
-				$new_value.="`".mysqli_real_escape_string($value[$i])."`";
-				if($i<count($value)){
-					$new_value.=",";
-				}
-			}
-		}
-		else{
-			$new_value="`".mysqli_real_escape_string($value)."`";
-		}
-		$qstring="SELECT ".$new_value." FROM `".mysqli_real_escape_string($table)."`";
-        if($params&&!$params%3){
-            for($i=0;$i<count($params);$i+=3){
-                $qstring.=" `".mysqli_real_escape_string($params[$i])."`".$params[$i+1]."'".mysqli_real_escape_string($params[$i+2])."'";
-            }
-        }
-        $qstring.=";";
-        $result=$this->db_query($qstring);
+        $result=$this->db_query($this->db_formfetchquery($table,$fields,$params));
         if($GLOBALS["MANDRIGO_CONFIG"]["DEBUG_MODE"]){
             $tmp_value=mysqli_result($result,$row);
         }
@@ -130,29 +110,10 @@ class db{
     
     //function to find an array of values from the database
     //returns the array of values
-    function db_fetcharray($table,$value,$params,$type="ASSOC"){
+    function db_fetcharray($table,$fields,$params,$type="ASSOC"){
 		$tmp_value="";
-		$new_value="";
-		if(ereg(",",$value)){
-			$value=explode($value);
-			for($i=0;$i<count($value);$i++){
-				$new_value.="`".mysqli_real_escape_string($value[$i])."`";
-				if($i<count($value)){
-					$new_value.=",";
-				}
-			}
-		}
-		else{
-			$new_value="`".mysqli_real_escape_string($value)."`";
-		}
-		$qstring="SELECT ".$new_value." FROM `".mysqli_real_escape_string($table)."`";
-        if($params&&!$params%3){
-            for($i=0;$i<count($params);$i+=3){
-                $qstring.=" `".mysqli_real_escape_string($params[$i])."`".$params[$i+1]."'".mysqli_real_escape_string($params[$i+2])."'";
-            }
-        }
-        $qstring.=";";
-        $result=$this->db_query($qstring);
+		
+        $result=$this->db_query($this->db_formfetchquery($table,$fields,$params));
         if($type="ASSOC"){
 			$type=MYSQLI_ASSOC;
 		}
@@ -173,19 +134,186 @@ class db{
         $this->db_freeresult($result);
         return $tmp_value;
 	}
-		
-    //function to find the number of rows in a query
+	
+	//function to execute non-get db querys
+	//returns true if the query was successful
+	function db_update($q_type,$table,$set,$params=""){
+		$qstring="";
+		switch($q_type){
+			case DB_UPDATE:
+				$qstring.="UPDATE `".mysqli_real_escape_string($table)."` SET";
+				for($i=0;$i<count($set);$i++){
+					$qstring.=" `".mysqli_real_escape_string($set[$i][0])."`='".mysqli_real_escape_string($set[$i][1])."'";
+					if($i<count($set)-1){
+						$qstring.=",";	
+					}
+				}
+				$qstring.=" WHERE";
+				for($i=0;$i<count($params);$i++){
+					$qstring.=" `".mysqli_real_escape_string($params[$i][0])."`".$params[$i][1]."'".mysqli_real_escape_string($params[$i][2])."'"; 	 
+					if($i<count($params)-1){
+						$qstring.=" AND";	
+					}					
+				}
+			break;
+			case DB_INSERT:
+				$qstring.="INSERT INTO `".mysqli_real_escape_string($table)."` VALUES (";
+				for($i=0;$i<count($set);$i++){
+					$qstring.="'".mysqli_real_escape_string($set[$i])."'";	
+					if($i<count($set)-1){
+						$qstring.=",";	
+					}
+				}
+				$qstring.=")";	
+			break;	
+			case DB_REMOVE:
+				$qstring.="DELETE FROM `".mysqli_real_escape_string($table)."` WHERE";
+	            for($i=0;$i<count($params);$i++){
+	              	$qstring.=" `".mysqli_real_escape_string($params[$i][0])."`".$params[$i][1]."'".mysqli_real_escape_string($params[$i][2])."'";
+	            	switch($params[$i][3]){
+						case DB_AND;
+							$qstring.=" AND";
+						break;
+						case DB_OR:
+							$qstring.=" OR";					
+						break;
+						default:
+							$qstring.="";
+						break;
+					};
+				}
+			break;
+		};
+		$qstring.=";";
+		if($this->db_query($qstring)){
+			$this->db_freeresult($result);
+			return true;	
+		}
+		return false;
+	}
+	
+	//function to preform DROP, CREATE, ALTER, and TRUNCATE sql statements
+	//returns true if query is a success
+	function db_dbcommands($c_type,$q_type,$db,$table="",$fields="",$params=""){
+	  	$qstring.="";
+		switch($c_type){
+			case DB_DROP:
+				if($q_type==DB_DATABASE){
+					$qstring.="DROP DATABASE `".mysqli_real_escape_string($db)."`";
+				}
+				else if($q_type==DB_TABLE){
+					$qstring.="DROP TABLE `".mysqli_real_escape_string($table)."`";
+				}				
+			break;
+			case DB_CREATE:
+				if($q_type==DB_DATABASE){
+					$qstring.="CREATE DATABASE `".mysqli_real_escape_string($db)."`";
+				}
+				else if($q_type==DB_TABLE){
+					$qstring.="CREATE TABLE `".mysqli_real_escape_string($table)."` (";
+					for($i=0;$i<count($fields);$i++){
+						$qstring.="`".mysqli_real_escape_string($fields[$i][0])."` ";
+						$qstring.=mysqli_real_escape_string($fields[$i][1])."(".mysqli_real_escape_string($fields[$i][2]).") ";
+						if($fields[$i][3]==DB_NULL){
+							$qstring.="default NULL";	  
+						}
+						else if($fields[$i][3]==DB_AUTO_INC){
+							$qstring.="NOT NULL  auto_increment";	
+						}
+						else{
+							$qstring.="NOT NULL default '".mysqli_real_escape_string($fields[$i][4])."'";
+						}
+						if($i<count($fields)-1){
+							$qstring.=",";
+						}
+					}
+					if($params){
+						$qstring.=",";
+					}
+					if($params){
+						for($i=0;$i<count($params);$i++){
+							if($params[$i][0]==DB_PRIMARY){
+								$qstring.="PRIMARY KEY (`".mysqli_real_escape_string($params[$i][1])."`)"; 
+							}
+							else if($params[$i][0]==DB_UINDEX){
+								$qstring.="UNIQUE KEY `".mysqli_real_escape_string($params[$i][1])."` (`".mysqli_real_escape_string($params[$i][2])."`)";   
+							}
+							else{
+								$qstring.="KEY `".mysqli_real_escape_string($params[$i][1])."` (`".mysqli_real_escape_string($params[$i][2])."`)"; 					
+							}
+							if($i<count($fields)-1){
+								$qstring.=",";
+							}
+						}
+					}
+					$qstring.=")"; 
+				}
+			break;
+			case DB_ALTER:
+				$qstring.="ALTER TABLE `".mysqli_real_escape_string($table)."`";
+				if($q_type==DB_DROP){
+					for($i=0;$i<count($fields);$i++){
+					  	if($fields[$i][0]==DB_TABLE){
+							$qstring.=" DROP `".mysqli_real_escape_string($fields[$i][1])."`";	
+						}
+						else if($fields[$i][0]==DB_PRIMARY){
+							$qstring.=" DROP PRIMARY KEY";
+						}
+						else{
+							$qstring.=" DROP INDEX `".mysqli_real_escape_string($fields[$i][1])."`";
+						}
+						if($i<count($fields)-1){
+							$qstring.=",";	
+						}
+					} 
+				}
+				if($q_type==DB_ADD){
+					for($i=0;$i<count($fields);$i++){
+					  	if($fields[$i][0]==DB_PRIMARY){
+							$qstring.=" ADD PRIMARY KEY(`".mysqli_real_escape_string($fields[$i][1])."`)";
+						}
+						else if($fields[$i][0]==DB_KEY){
+							$qstring.=" ADD INDEX(`".mysqli_real_escape_string($fields[$i][1])."`)";
+						}
+						else if($fields[$i][0]==DB_UINDEX){
+							$qstring.=" ADD UNIQUE(`".mysqli_real_escape_string($fields[$i][1])."`)";	
+						}
+					  	else{
+							$qstring.=" ADD `".mysqli_real_escape_string($fields[$i][0])."` ";
+							$qstring.=mysqli_real_escape_string($fields[$i][1])."(".mysqli_real_escape_string($fields[$i][2]).") ";
+							if($fields[$i][3]==DB_NULL){
+								$qstring.="default NULL";	  
+							}
+							else if($fields[$i][3]==DB_AUTO_INC){
+								$qstring.="NOT NULL  auto_increment";	
+							}
+							else{
+								$qstring.="NOT NULL default '".mysqli_real_escape_string($fields[$i][4])."'";
+							}
+						}	
+						if($i<count($fields)-1){
+							$qstring.=",";	
+						}
+					} 				  
+				}
+			break;
+			case DB_TRUNCATE:
+				$qstring.="TRUNCATE TABLE `".mysqli_real_escape_string($table)."`";
+			break;
+		};
+		$qstring.=";";
+		if($this->db_query($qstring)){
+			$this->db_freeresult($result);
+			return true;	
+		}
+		return false;
+	}
+
+   //function to find the number of rows in a query
     //returns the number of rows
     function db_numrows($table,$params){
         $num_rows=0;
-        $qstring="SELECT * FROM `".mysqli_real_escape_string($table)."`";
-        if($params&&!$params%3){
-            for($i=0;$i<count($params);$i+=3){
-                $qstring.=" `".mysqli_real_escape_string($params[$i])."`".$params[$i+1]."'".mysqli_real_escape_string($params[$i+2])."'";
-            }
-        }
-        $qstring.=";";
-        $result=$this->db_query($qstring);
+        result=$this->db_query($this->db_formfetchquery($table,"",$params));
         if($GLOBALS["MANDRIGO_CONFIG"]["DEBUG_MODE"]){
             $num_rows=mysqli_num_rows($result);
         }
@@ -202,14 +330,7 @@ class db{
     //returns the number of fields
 	function db_numfields($table,$params){
 		$num_cols=0;
-		$qstring="SELECT * FROM ".mysqli_real_escape_string($table);
-        if($params&&!$params%3){
-            for($i=0;$i<count($params);$i+=3){
-                $qstring.=" ".mysqli_real_escape_string($params[$i]).$params[$i+1].mysqli_real_escape_string($params[$i+2]);
-            }
-        }
-        $qstring.=";";
-        $result=$this->db_query($qstring);
+        result=$this->db_query($this->db_formfetchquery($table,"",$params));
         if($GLOBALS["MANDRIGO_CONFIG"]["DEBUG_MODE"]){
             $num_cols=mysqli_num_fields($result);
         }
@@ -222,7 +343,7 @@ class db{
         return $num_cols;			
 	}
 	
-    //internal only query function.  Should only be called by functions in the db class!!
+    //Basic query function.  This should be NOT used for external (outside this class) operations unless absolutly necessary.
     function db_query($string){
         $result="";
         if($GLOBALS["MANDRIGO_CONFIG"]["DEBUG_MODE"]){
@@ -275,4 +396,82 @@ class db{
             @mysqli_free_result($result);
         }
     }
+    
+    //Internal function to form querys for select statements
+    //returns the query string
+    function db_formfetchquery($table,$fields,$params){
+		$new_field="";
+		$new_table="";
+		if(ereg(",",$fields)){
+			$fields=explode($fields);
+			for($i=0;$i<count($fields);$i++){
+				$new_field.="`".mysqli_real_escape_string($fields[$i])."`";
+				if($i<count($fields)){
+					$new_field.=",";
+				}
+			}
+		}
+		else{
+		  	if($fields){
+				$new_field="`".mysqli_real_escape_string($fields)."`";
+			}
+			else{
+				$new_field.="*";
+			}
+		}
+		if(ereg(",",$table)){
+			$table=explode($table);
+			for($i=0;$i<count($table);$i++){
+				$new_table.="`".mysqli_real_escape_string($table[$i])."`";
+				if($i<count($table)){
+					$new_table.=",";
+				}
+			}
+		}
+		else{
+			$new_table="`".mysqli_real_escape_string($table)."`";
+		}
+		$qstring="SELECT ".$new_field." FROM ".$new_table;
+        if($params){
+          	$qstring.=" WHERE";
+            for($i=0;$i<count($params);$i++){
+              	switch($params[$i][0]){
+              	  	case DB_IN:
+              	  		$qstring.=" `".mysqli_real_escape_string($params[$i][1])."` IN (";
+              	  		for($j=0;$j<count($params[$i][2]);$j++){
+							$qstring.="'".mysqli_real_escape_string($params[$i][2][$j])."'";
+							if($j<count($params[$i][2])-1){
+								$qstring.=",";
+							}
+						}
+						$qstring.=")";
+              	  	break;
+              	  	case DB_BETWEEN:
+              	  		$qstring.=" `".mysqli_real_escape_string($params[$i][1])."` BETWEEN '";
+						$qstring.=mysqli_real_escape_string($params[$i][2][0])."' AND '".mysqli_real_escape_string($params[$i][2][1])."'";
+								
+              	  	break;
+				 	default:  
+						$qstring.=" `".mysqli_real_escape_string($params[$i][0])."`".$params[$i][1]."'".mysqli_real_escape_string($params[$i][2])."'";
+				    break;
+				};
+            	switch($params[$i][3]){
+					case DB_AND;
+						$qstring.=" AND";
+					break;
+					case DB_OR:
+						$qstring.=" OR";					
+					break;
+					default:
+						$qstring.="";
+					break;
+				};
+			}
+        }
+        else{
+			$qstring.=" WHERE 1";
+		}
+        return $qstring.";";
+	}
+}
 }
