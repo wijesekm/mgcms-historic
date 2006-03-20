b7a838b3bd436ca0fbc6b42d3533b4c97049715b
kwijesekera <kwijesekera@ddaff062-d30e-0410-924e-b8927a9d2c55>
2006-03-20 20:28:41 +0000

    initial checkin

---
+++ trunk/login_manager/inc/auth/pam_auth.class.php
@@ -0,0 +1,78 @@
+<?php
+/**********************************************************
+    pam_auth.class.php
+	Last Edited By: Kevin Wijesekera
+	Date Last Edited: 03/20/06
+
+	Copyright (C) 2006  Kevin Wijesekera
+
+    ##########################################################
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+	###########################################################
+
+**********************************************************/
+
+//
+//To prevent direct script access
+//
+if(!defined("START_MANDRIGO")){
+    die("<html><head>
+            <title>Forbidden</title>
+        </head><body>
+            <h1>Forbidden</h1><hr width=\"300\" align=\"left\"/>\n<p>You do not have permission to access this file directly.</p>
+        </html></body>");
+}
+
+class auth extends _auth{
+  
+  	var $sql_db;
+  
+	function auth($db){
+	 	$this->sql_db=$db; 
+	}
+	function auth_validate($user_name,$user_password,$crypt_type){
+	  
+	  	//username verification
+	  	if(!$this->auth_cleanusername($user_name)){
+			return false;
+		}
+		//password verification
+		if(!$this->auth_cleanpassword($user_password)){
+			return false;
+		}
+		if(pam_auth($user_name,get_magic_quotes_gpc()?stripslashes($user_password):$user_password){
+			return true;
+		}
+		return false;
+	}
+	function auth_loguserin($user_name,$ip,$timestamp){
+		return $this->sql_db->db_update(DB_UPDATE,TABLE_PREFIX.TABLE_USER_DATA,array(array("user_last_login",$timestamp),array("user_last_ip",$ip)),array(array("user_name","=",$user_name))));
+	}
+	function auth_validsession($uid,$session){
+	  	if(!$this->auth_cleanuid($uid)){
+			return false;
+		}	  	
+		if(!$uid||$uid==1){
+			return false;
+		}
+		$user_data=$this->sql_db->db_fetcharray(TABLE_PREFIX.TABLE_USER_DATA,"",array(array("user_id","=",$uid)));
+		if($user_data["user_session"]===$session){
+			return true;
+		}
+		return false;
+	}
+}
+?>
