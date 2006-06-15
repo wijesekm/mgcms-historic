98cca7a6793360af0cb03a91f4320497ac0570c7
kwijesekera <kwijesekera@ddaff062-d30e-0410-924e-b8927a9d2c55>
2006-06-15 02:29:37 +0000

    Updated the var checking function to work with the new var parsing system

--- trunk/mandrigo/inc/ini/clean_functions.ini.php
+++ trunk/mandrigo/inc/ini/clean_functions.ini.php
@@ -2,9 +2,9 @@
 /**********************************************************
     clean_functions.ini.php
 	Last Edited By: Kevin Wijesekera
-	Date Last Edited: 11/05/05
+	Date Last Edited: 05/08/06
 
-	Copyright (C) 2005  Kevin Wijesekera
+	Copyright (C) 2006  Kevin Wijesekera
 
     ##########################################################
 	This program is free software; you can redistribute it and/or
@@ -35,41 +35,44 @@ if(!defined("START_MANDRIGO")){
             <h1>Forbidden</h1><hr width=\"300\" align=\"left\"/>\n<p>You do not have permission to access this file directly.</p>
         </html></body>");
 }
-
-function clean_num($num){
-    return (eregi("^[0-9]+$",$num))?$num:BAD_DATA;
-}
-function clean_SESID($id){
-    return (eregi("^[0-9a-z]+$",$id))?$id:BAD_DATA;
-}
-function clean_UID($id){
-    return (eregi("^[0-9]+$",$id))?$id:BAD_DATA;
-}
-function clean_email($string){
-    return (eregi("^[a-z0-9._-]+@[a-z0-9-]+\.[a-z.]{2,5}$",$string))?$string : BAD_DATA;
-}
-function clean_id($string){
-    return (eregi("^[a-z]{0,1}[0-9]+$",$string))?$string:BAD_DATA;
-}
-function clean_page($string){
-    return (eregi("^[a-z0-9._-]+$",$string))?$string:BAD_DATA;
-}
-function clean_action($string){
-    return (eregi("^[a-z]+$",$string))?$string:BAD_DATA;
-}
-function clean_password($string){
-    return (eregi("^[a-z0-9._-]+$",$string))?$string:BAD_DATA;
-}
-function clean_name($string){
-    return (eregi("[[:space:]a-z.,]",$string))?$string:BAD_DATA;
-}
-function clean_text($string){
-    return (!eregi("[<|>]",$string))?$string:BAD_DATA;
-}
-function clean_username($string){
-    return (eregi("^[a-z0-9._-]+$",$string))?$string:BAD_DATA;
-}
-function clean_url($string){
-	return (eregi("[<|>[|]{|}]",$string))?BAD_DATA:$string;
+function clean_var($string,$method){
+	switch($method){
+		case "num":
+			return (eregi("^[0-9]+$",$string))?$string:BAD_DATA;
+		break;
+		case "sesid":
+			return (eregi("^[0-9a-z]+$",$string))?$string:BAD_DATA;
+		break;	
+		case "uid":  
+			return (eregi("^[0-9]+$",$string))?$string:BAD_DATA;
+		break;
+		case "email":
+			return (eregi("^[a-z0-9._-]+@[a-z0-9-]+\.[a-z.]{2,5}$",$string))?$string:BAD_DATA;
+		break;
+		case "id":
+			return (eregi("^[a-z]{0,1}[0-9]+$",$string))?$string:BAD_DATA;
+		break;
+		case "page":
+			return (eregi("^[a-z0-9._-]+$",$string))?$string:BAD_DATA;
+		break;
+		case "action":
+			return (eregi("^[a-z]+$",$string))?$string:BAD_DATA;
+		break;
+		case "password":
+			return (eregi("^[a-z0-9._-]+$",$string))?$string:BAD_DATA;
+		break;
+		case "name":
+			return (eregi("[[:space:]a-z.,]",$string))?$string:BAD_DATA;
+		break;
+		case "text":
+			return (!eregi("[<|>]",$string))?$string:BAD_DATA;
+		break;
+		case "username":
+			return (eregi("^[a-z0-9._-]+$",$string))?$string:BAD_DATA;
+		break;
+		case "url":
+			return (eregi("[<|>[|]{|}]",$string))?BAD_DATA:$string;
+		break;
+	};
 }
 ?>
