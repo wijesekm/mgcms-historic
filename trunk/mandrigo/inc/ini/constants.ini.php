c1b557412c23c9856b21d9087f5373eadfe6bfe1
kwijesekera <kwijesekera@ddaff062-d30e-0410-924e-b8927a9d2c55>
2007-02-28 21:42:49 +0000

    Added more database stuff.

--- trunk/mandrigo/inc/ini/constants.ini.php
+++ trunk/mandrigo/inc/ini/constants.ini.php
@@ -43,8 +43,8 @@ define("TABLE_PAGES","pages");
 define("TABLE_ACL","acl");
 define("TABLE_LANGSETS","langsets");
 define("TABLE_LANG","lang");
+define("TABLE_PACKAGES","packages");
 
-define("TABLE_PACKAGE_DATA","packages");
 define("TABLE_SITE_STATS","site_stats");
 define("TABLE_CAPTCHA","captcha");
 define("TABLE_CAPTCHA_DATA","captcha_data");
@@ -97,4 +97,3 @@ define("MULTIPART_ALT","multipart/alternative");
 define("TEXT_PLAIN","text/plain");
 define("TEXT_HTML","text/html");
 define("MULTI_ALT","multipart/alternative");
-define("CORE_PACKAGES","mg_packages");
