fda3f9e2e0361501694f55ab35e5ef9ee24164b8
kwijesekera <kwijesekera@ddaff062-d30e-0410-924e-b8927a9d2c55>
2007-03-15 00:21:46 +0000

    Initial Revision.  (copy of ini.php minus package loading)

---
+++ trunk/admin/inc/ini/ini.admin.php
@@ -0,0 +1,178 @@
+<?php
+/**********************************************************
+    ini.php
+	Last Edited By: Kevin Wijesekera
+	Date Last Edited: 01/31/07
+
+	Copyright (C) 2006-2007 the MandrigoCMS Group
+
+    ##########################################################
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+	###########################################################
+
+**********************************************************/
+
+//
+//To prevent direct script access
+//
+if(!defined("START_MANDRIGO")){
+    die($GLOBALS["MANDRIGO"]["CONFIG"]["DIE_STRING"]);
+}
+
+//
+//PHP INI varables
+//
+
+//turns off magic quotes runtime
+@set_magic_quotes_runtime(0);
+
+//sets php error reporting
+if($GLOBALS["MANDRIGO"]["CONFIG"]["DEBUG_MODE"]){
+	@error_reporting(E_ALL);
+}
+else{
+	@error_reporting(0);
+}
+
+//
+//Add slashes to the input varable arrays POST, GET, and COOKIE if magic_quotes_gpc not set
+//
+if(!get_magic_quotes_gpc()){
+	if(is_array($HTTP_GET_VARS)){
+		while(list($k, $v)=each($HTTP_GET_VARS)){
+			if(is_array($HTTP_GET_VARS[$k])){
+				while(list($k2, $v2)=each($HTTP_GET_VARS[$k])){
+					$HTTP_GET_VARS[$k][$k2]=addslashes($v2);
+				}
+				@reset($HTTP_GET_VARS[$k]);
+			}
+			else{
+				$HTTP_GET_VARS[$k]=addslashes($v);
+			}
+		}
+		@reset($HTTP_GET_VARS);
+	}
+
+	if(is_array($HTTP_POST_VARS)){
+		while(list($k, $v)=each($HTTP_POST_VARS)){
+			if(is_array($HTTP_POST_VARS[$k])){
+				while(list($k2, $v2)=each($HTTP_POST_VARS[$k])){
+					$HTTP_POST_VARS[$k][$k2]=addslashes($v2);
+				}
+				@reset($HTTP_POST_VARS[$k]);
+			}
+			else{
+				$HTTP_POST_VARS[$k]=addslashes($v);
+			}
+		}
+		@reset($HTTP_POST_VARS);
+	}
+
+	if(is_array($HTTP_COOKIE_VARS)){
+		while(list($k, $v)=each($HTTP_COOKIE_VARS)){
+			if(is_array($HTTP_COOKIE_VARS[$k])){
+				while(list($k2, $v2)=each($HTTP_COOKIE_VARS[$k])){
+					$HTTP_COOKIE_VARS[$k][$k2]=addslashes($v2);
+				}
+				@reset($HTTP_COOKIE_VARS[$k]);
+			}
+			else{
+				$HTTP_COOKIE_VARS[$k]=addslashes($v);
+			}
+		}
+		@reset($HTTP_COOKIE_VARS);
+	}
+}
+
+//
+// If install has not been compleated we will forward to the install page
+//
+if(!$GLOBALS["MANDRIGO"]["CONFIG"]["IS_INSTALLED"]){
+    header("Location: ../install/install.$php_ex");
+    die();
+}
+
+//
+// Now we will start up SQL and connect to the server/database
+//
+if($GLOBALS["MANDRIGO"]["CONFIG"]["DEBUG_MODE"]){
+    require_once($GLOBALS["MANDRIGO"]["CONFIG"]["ROOT_PATH"]."db{$GLOBALS["MANDRIGO"]["CONFIG"]["PATH"]}".$sql_config["SQL_TYPE"].".class.$php_ex");
+}
+else{
+    if(!(@include_once($GLOBALS["MANDRIGO"]["CONFIG"]["ROOT_PATH"]."db{$GLOBALS["MANDRIGO"]["CONFIG"]["PATH"]}".$sql_config["SQL_TYPE"].".class.$php_ex"))){
+        $GLOBALS["MANDRIGO"]["ERROR_LOGGER"]->el_adderror(3,"core");
+	   	die($GLOBALS["MANDRIGO"]["ELOG"]["HTMLHEAD"].$GLOBALS["MANDRIGO"]["ELOG"]["TITLE"].$GLOBALS["MANDRIGO"]["ELOG"]["HTMLBODY"].
+           	$GLOBALS["MANDRIGO"]["ERROR_LOGGER"]->el_generatereport().$GLOBALS["MANDRIGO"]["ELOG"]["HTMLEND"]);
+    }
+}
+$GLOBALS["MANDRIGO"]["DB"] = & new db();
+
+if($GLOBALS["MANDRIGO"]["CONFIG"]["DEBUG_MODE"]){
+	$GLOBALS["MANDRIGO"]["DB"]->db_connect($sql_config["SQL_HOST"],$sql_config["SQL_PORT"],$sql_config["SQL_SOCKET"],$sql_config["SQL_USER"],
+						$sql_config["SQL_PASSWORD"],$sql_config["SQL_DATABASE"],true,$sql_config["USE_SSL"],$sql_config["SSL"]);
+}
+else{
+    if(!$GLOBALS["MANDRIGO"]["DB"]->db_connect($sql_config["SQL_HOST"],$sql_config["SQL_PORT"],$sql_config["SQL_SOCKET"],$sql_config["SQL_USER"],
+		$sql_config["SQL_PASSWORD"],$sql_config["SQL_DATABASE"],true,$sql_config["USE_SSL"],$sql_config["SSL"])){
+        $GLOBALS["MANDRIGO"]["ERROR_LOGGER"]->el_adderror(2,"sql");
+	   	die($GLOBALS["MANDRIGO"]["ELOG"]["HTMLHEAD"].$GLOBALS["MANDRIGO"]["ELOG"]["TITLE"].$GLOBALS["MANDRIGO"]["ELOG"]["HTMLBODY"].
+           	$GLOBALS["MANDRIGO"]["ERROR_LOGGER"]->el_generatereport().$GLOBALS["MANDRIGO"]["ELOG"]["HTMLEND"]);
+    }
+}
+
+
+
+
+//
+//Gets rid of unneeded config vars
+//
+$sql_config="";
+$log_config="";
+$adldap_config="";
+//
+//Seeds random number generator
+//
+srand(((int)((double)microtime()*1000003)));
+mt_srand(doubleval(microtime()) * 1000003);
+
+//
+//Init Script
+//
+function package_init($pkg,$root=true){
+	$pkg_size=count($pkg);
+	if($root){
+		$base=$GLOBALS["MANDRIGO"]["CONFIG"]["ROOT_PATH"];
+	}
+	else{
+		$base=$GLOBALS["MANDRIGO"]["CONFIG"]["LOGIN_ROOT_PATH"];
+	}
+	for($pkg_c=0;$pkg_c<$pkg_size;$pkg_c++){
+	 	if($pkg[$pkg_c][0]){
+			if($GLOBALS["MANDRIGO"]["CONFIG"]["DEBUG_MODE"]){
+				include_once($base.$pkg[$pkg_c][0]);
+			}
+			else{
+				if(!(@include_once($base.$pkg[$pkg_c][0]))){
+					$GLOBALS["MANDRIGO"]["ERROR_LOGGER"]->el_adderror($pkg[$pkg_c][1],"core");
+				}
+			}	
+		}
+	}
+    if($GLOBALS["MANDRIGO"]["ERROR_LOGGER"]->el_getstatus()==2){
+	   	die($GLOBALS["MANDRIGO"]["ELOG"]["HTMLHEAD"].$GLOBALS["MANDRIGO"]["ELOG"]["TITLE"].$GLOBALS["MANDRIGO"]["ELOG"]["HTMLBODY"].
+           	$GLOBALS["MANDRIGO"]["ERROR_LOGGER"]->el_generatereport().$GLOBALS["MANDRIGO"]["ELOG"]["HTMLEND"]);
+    }
+}
