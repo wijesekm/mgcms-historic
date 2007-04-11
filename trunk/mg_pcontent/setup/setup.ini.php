6ab15a110b19e459e4f6d6b9f02a29081dd2ae26
kwijesekera <kwijesekera@ddaff062-d30e-0410-924e-b8927a9d2c55>
2007-04-11 14:43:17 +0000

    Added Setup File.

---
+++ trunk/mg_pcontent/setup/setup.ini.php
@@ -0,0 +1,52 @@
+<?php
+/**********************************************************
+    setup.ini.php
+    mg_pcontent ver 0.7.0
+	Last Edited By: Kevin Wijesekera
+	Date Last Edited: 04/11/07
+
+	Copyright (C) 2006-2007 the MandrigoCMS Group
+
+    ##########################################################
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+	###########################################################
+
+**********************************************************/
+
+//
+//To prevent direct script access
+//
+if(!defined("START_MANDRIGO")){
+    die($GLOBALS["MANDRIGO"]["CONFIG"]["DIE_STRING"]);
+}
+
+$pkg["name"]="mg_pcontent";
+$pkg["version"]="0.7.0";
+$pkg["maintainer"]="Kevin Wijesekera";
+$pkg["email"]="k_wijesekera@yahoo.com";
+$pkg["website"]="http://kevinwijesekera.net";
+
+$pkg["enabled"]=true;
+$pkg["no_load_error"]["id"]="500";
+$pkg["no_load_error"]["message"]="The mg_pcontent package could not be loaded.";
+
+//Do Not Edit Below This Line
+if($pkg["enabled"]){
+	$pkg["enabled"]="E";	
+}
+else{
+	$pkg["enabled"]="D";	
+}
