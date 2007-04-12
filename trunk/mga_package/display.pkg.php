f9bbcc10dcc7c7cae0609790c8696b146f6a8773
kwijesekera <kwijesekera@ddaff062-d30e-0410-924e-b8927a9d2c55>
2007-04-12 17:07:56 +0000

    Added the mga_package package.  This package is a required admin package that will take care of installing/removing/update/version checking other packages.

---
+++ trunk/mga_package/display.pkg.php
@@ -0,0 +1,432 @@
+<?php
+/**********************************************************
+    display.pkg.php
+    mga_package ver 0.7.0
+	Last Edited By: Kevin Wijesekera
+	Date Last Edited: 04/11/07
+
+	Copyright (C) 2006-2007 the MandrigoCMS Group
+
+    ##########################################################
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+	###########################################################
+
+**********************************************************/
+
+//
+//To prevent direct script access
+//
+if(!defined("START_MANDRIGO")){
+    die($GLOBALS["MANDRIGO"]["CONFIG"]["DIE_STRING"]);
+}
+
+class package_admin{
+	
+	var $tpl;
+    //for xml parsing
+   	var $document;
+   	var $curr_tag;
+   	var $tag_stack;
+	   	
+	function package_admin($i){
+	    $file=$GLOBALS['MANDRIGO']['CONFIG']['TEMPLATE_PATH'].$GLOBALS['MANDRIGO']['CURRENTAPAGE']['DATAPATH'].$GLOBALS['MANDRIGO']['CURRENTAPAGE']['NAME'].'_'.$i.'.'.TPL_EXT;
+
+		switch($GLOBALS["MANDRIGO"]["VARS"]["ACTION"]){
+			default:
+				$this->tpl=new template();
+				if(!$this->tpl->tpl_load($file,"index")||!$this->tpl->tpl_load($file,"index_item")||!$this->tpl->tpl_load($file,"install_item")){
+					$GLOBALS["MANDRIGO"]["ERROR_LOGGER"]->el_adderror(2001,"display");
+					return false;					
+				}
+			break;
+		};
+		return true;
+	}
+	function pa_display(){
+	 	$string="";
+		switch($GLOBALS["MANDRIGO"]["VARS"]["ACTION"]){
+		 	case "add":
+		 		include($GLOBALS["MANDRIGO"]["CONFIG"]["PLUGIN_PATH"].$GLOBALS["MANDRIGO"]["VARS"]["PACKAGE"].SETUP_FOLDER.SETUP_NAME);
+		 		if($pkg["name"]){
+					$GLOBALS["MANDRIGO"]["DB"]->db_update(DB_INSERT,TABLE_PREFIX.TABLE_PACKAGES,array($pkg["name"],$pkg["no_load_error"],$pkg["version"],$pkg["maintainer"],$pkg["email"],$pkg["website"],($pkg["enabled"])?"E":"D"),array("pkg_name","pkg_nlerror","pkg_ver","pkg_maintainer","pkg_email","pkg_web","pkg_status"));
+					$pid=$GLOBALS["MANDRIGO"]["DB"]->db_fetchresult(TABLE_PREFIX.TABLE_PACKAGES,"pkg_id",array(array("pkg_name","=",$pkg["name"])));
+					$this->pa_changelog($pkg["errors"],true);
+					$this->pa_updatelang($pkg["languages"],$pkg_language_install,$pid,true);
+					$this->pa_updatedb($pkg["tables"],$pkg_table_install,true);
+				}
+				$string=$this->pa_genoverview();
+		 	break;
+		 	case "remove":
+		 		if($GLOBALS["MANDRIGO"]["VARS"]["PACKAGE"]&&$GLOBALS["MANDRIGO"]["VARS"]["PACKAGE"]>0){
+		 		 	$package=$GLOBALS["MANDRIGO"]["DB"]->db_fetcharray(TABLE_PREFIX.TABLE_PACKAGES,"",array(array("pkg_id","=",$GLOBALS["MANDRIGO"]["VARS"]["PACKAGE"])));
+		 			$GLOBALS["MANDRIGO"]["DB"]->db_update(DB_REMOVE,TABLE_PREFIX.TABLE_PACKAGES,"",array(array("pkg_id","=",$GLOBALS["MANDRIGO"]["VARS"]["PACKAGE"])));
+		 			include($GLOBALS["MANDRIGO"]["CONFIG"]["PLUGIN_PATH"].$package["pkg_name"].SETUP_FOLDER.SETUP_NAME);
+		 			$this->pa_changelog($pkg["errors"]);
+		 			$this->pa_updatelang($pkg["languages"],$pkg_language_install,0,false);
+		 			$this->pa_updatedb($pkg["tables"],$pkg_table_install,false);
+				}
+		 		$string=$this->pa_genoverview();
+		 	break;
+		 	case "disable":
+		 		if($GLOBALS["MANDRIGO"]["VARS"]["PACKAGE"]&&$GLOBALS["MANDRIGO"]["VARS"]["PACKAGE"]>0){
+					$package=$GLOBALS["MANDRIGO"]["DB"]->db_fetcharray(TABLE_PREFIX.TABLE_PACKAGES,"",array(array("pkg_id","=",$GLOBALS["MANDRIGO"]["VARS"]["PACKAGE"])));
+					if($package["pkg_status"]=="E"){
+						$GLOBALS["MANDRIGO"]["DB"]->db_update(DB_UPDATE,TABLE_PREFIX.TABLE_PACKAGES,array(array("pkg_status","D")),array(array("pkg_id","=",$GLOBALS["MANDRIGO"]["VARS"]["PACKAGE"])));
+					}
+					else if($package["pkg_status"]=="D"){
+						$GLOBALS["MANDRIGO"]["DB"]->db_update(DB_UPDATE,TABLE_PREFIX.TABLE_PACKAGES,array(array("pkg_status","E")),array(array("pkg_id","=",$GLOBALS["MANDRIGO"]["VARS"]["PACKAGE"])));						
+					}
+				}
+				$string=$this->pa_genoverview();
+		 	break;
+			default:
+				$string=$this->pa_genoverview();
+			break;
+		};
+		return $string;
+	}
+	function pa_genoverview(){
+		$string="";
+		$this->pa_xmlparse(PACKAGE_VER_PREFIX.$GLOBALS["MANDRIGO"]["SITE"]["UPDATE_SERVER"].PACKAGE_FILE);
+		$this->document=$this->document["MANDRIGO_PACKAGES"][0];
+		$packages=$GLOBALS["MANDRIGO"]["DB"]->db_fetcharray(TABLE_PREFIX.TABLE_PACKAGES,"",array(array(DB_ORDERBY,"pkg_name","ASC")),"ASSOC",DB_ALL_ROWS);
+		$soq=count($packages);
+		$names=array();
+		for($i=0;$i<$soq;$i++){
+			$names[$i]=$packages[$i]["pkg_name"];				 
+				 	
+			//
+			//Parsing
+			//
+			$status="";
+			$keys=array_keys($this->document);
+			if(in_array(mb_strtoupper($packages[$i]["pkg_name"]),$keys)){
+				if($this->pa_versioncomp($this->document[mb_strtoupper($packages[$i]["pkg_name"])][0]["VERSION"][0]["data"],$packages[$i]["pkg_ver"])){
+					$status="<acronym title=\"Up-To-Date\">OK</acronym>";
+				}
+				else{
+					$status="<acronym title=\"Needs Updating\">NU</acronym>";
+				}
+			}
+			else{
+				$status="<acronym title=\"Unknown\">U</acronym>";
+			}
+			if($packages[$i]["pkg_status"]=="E"){
+				$dname="<acronym title=\"Disable\">D</acronym>";
+			}
+			else{
+				$dname="<acronym title=\"Enable\">E</acronym>";
+			}
+			//
+			//Display
+			//
+
+			$tpl_item=new template();
+			$tpl_item->tpl_load($this->tpl->tpl_return("index_item"),"item",false);
+			if($packages[$i]["pkg_id"]<1){
+				$parse=array("ID",$packages[$i]["pkg_id"],"NAME",$packages[$i]["pkg_name"],"STATUS",$status
+						,"DISABLE_URL",""
+						,"REMOVE_URL","");
+			}
+			else{
+				$parse=array("ID",$packages[$i]["pkg_id"],"NAME",$packages[$i]["pkg_name"],"STATUS",$status
+						,"DISABLE_URL",$this->pa_genlink(array("pa",$GLOBALS["MANDRIGO"]["CURRENTAPAGE"]["NAME"],"pkg",$packages[$i]["pkg_id"],"a","disable"),$dname)
+						,"REMOVE_URL",$this->pa_genlink(array("pa",$GLOBALS["MANDRIGO"]["CURRENTAPAGE"]["NAME"],"pkg",$packages[$i]["pkg_id"],"a","remove"),"X",true,"Remove Package? Data will be lost!"));	
+			}
+			$tpl_item->tpl_parse($parse,"item",1,false);
+			$string.=$tpl_item->tpl_return("item");
+		}
+		$upackages=scandir($GLOBALS["MANDRIGO"]["CONFIG"]["PLUGIN_PATH"]);
+		$soq=count($upackages);
+		$string2="";
+		for($i=0;$i<$soq;$i++){
+		 	//echo !in_array($upackages[$i],$names);
+			if(!in_array($upackages[$i],$names)&&$upackages[$i]!="."&&$upackages[$i]!=".."){
+				$tpl_item=new template();
+				$tpl_item->tpl_load($this->tpl->tpl_return("install_item"),"itemi",false);
+				$parse=array("NAME",$upackages[$i],"INSTALL",$this->pa_genlink(array("pa",$GLOBALS["MANDRIGO"]["CURRENTAPAGE"]["NAME"],"pkg",$upackages[$i],"a","add"),"X"));
+				$tpl_item->tpl_parse($parse,"itemi",1,false);
+				$string2.=$tpl_item->tpl_return("itemi");
+			}
+		}
+		$this->tpl->tpl_parse(array("PACKAGES",$string,"IPACKAGES",$string2),"index",1,false);
+		return $this->tpl->tpl_return("index"); 
+	}
+	function pa_versioncomp($reference,$local){
+		$reference=explode(".",$reference);
+		$local=explode(".",$local);
+		if($local[2]<$reference[2]||$local[1]<$reference[1]||$local[0]<$reference[0]){
+			return false;
+		}
+		return true;
+	}
+    function pa_genlink($url_data,$name,$conf=false,$conf_msg=""){
+      	$link='';
+ 		if($GLOBALS['MANDRIGO']['SITE']['URL_FORMAT']==1){
+			$url=$GLOBALS['MANDRIGO']['SITE']['ADMIN_URL'].$GLOBALS['MANDRIGO']['SITE']['ADMIN_NAME']."/";
+		}
+		else{
+		  	$url=$GLOBALS['MANDRIGO']['SITE']['ADMIN_URL'].$GLOBALS['MANDRIGO']['SITE']['ADMIN_NAME']."?";
+		}  
+		$soq=count($url_data);
+		$i=0;
+		while($i<$soq){
+			if($GLOBALS['MANDRIGO']['SITE']['URL_FORMAT']==1){
+				$url.=$url_data[$i]."/".$url_data[$i+1];
+				$i+=2;
+				if($i<$soq){
+					$url.="/";
+				}
+			}
+			else{
+				$url.=$url_data[$i]."=".$url_data[$i+1];
+				$i+=2;
+				if($i<$soq){
+					$url.="&amp;";
+				}
+			}
+		}
+		if($conf){
+			$url="javascript:if (confirm('$conf_msg')) window.location='$url';";
+		}
+		$link=ereg_replace("{ATTRIB}","href=\"".$url."\"",$GLOBALS["MANDRIGO"]["HTML"]["A"]);
+		return ereg_replace("{VALUE}",$name,$link);
+	}
+	
+	function pa_updatelang($keys,$lang,$pid,$add=false){
+		$solang=count($keys);
+		for($i=0;$i<$solang;$i++){
+			$soj=count($lang[$keys[$i]]);
+			$lang_data=$GLOBALS["MANDRIGO"]["DB"]->db_fetcharray(TABLE_PREFIX.TABLE_LANGSETS,"",array(array("lang_name","=",$keys[$i])));
+			for($j=0;$j<$soj;$j++){
+				if($add){
+					$GLOBALS["MANDRIGO"]["DB"]->db_update(DB_INSERT,TABLE_PREFIX.TABLE_LANG.$lang_data["lang_id"],array($lang[$keys[$i]][$j][0],$lang[$keys[$i]][$j][1],"mg_packages",$pid),array("lang_callname","lang_value","lang_corename","lang_appid"));		
+				}
+				else{
+					$GLOBALS["MANDRIGO"]["DB"]->db_update(DB_REMOVE,TABLE_PREFIX.TABLE_LANG.$lang_data["lang_id"],"",array(array("lang_callname","=",$lang[$keys[$i]][$j][0])));		
+				}
+			}
+		}
+	}
+	
+	function pa_updatedb($keys,$table,$add=false){
+		$sodb=count($keys);
+		for($i=0;$i<$sodb;$i++){
+		 	if($add){
+				$GLOBALS["MANDRIGO"]["DB"]->db_dbcommands(DB_CREATE,DB_TABLE,"",TABLE_PREFIX.$keys[$i],$table[$keys[$i]]["struct"],$table[$keys[$i]]["keys"]);
+				$soj=count($table[$keys[$i]]["records"]);
+				for($j=0;$j<$soj;$j++){
+				 	$tmp=$table[$keys[$i]]["records"][$j];
+					$GLOBALS["MANDRIGO"]["DB"]->db_update(DB_INSERT,TABLE_PREFIX.$keys[$i],$tmp[1],$tmp[0]);
+				}
+			}
+			else{
+				$GLOBALS["MANDRIGO"]["DB"]->db_dbcommands(DB_DROP,DB_TABLE,"",TABLE_PREFIX.$keys[$i]);
+			}
+		}
+	}
+	
+	function pa_changelog($array,$add=false){
+		$keys=array_keys($array);
+		$soi=count($keys);
+		for($i=0;$i<$soi;$i++){
+			$soj=count($array[$keys[$i]]);
+			$this->pa_xmlparse($GLOBALS["MANDRIGO"]["CONFIG"]["ROOT_PATH"].LOG_SETUP.$keys[$i].".".XML_EXT);
+			$this->document=$this->document["ERROR_LOG"][0]["MSG"];
+			if($array[$keys[$i]][0][0]){
+				for($j=0;$j<$soj;$j++){
+					$this->pa_editlogconf($array[$keys[$i]][$j][0],$add,$array[$keys[$i]][$j][1]);		
+				}
+				if($GLOBALS["MANDRIGO_CONFIG"]["DEBUG_MODE"]){
+					$f=fopen($GLOBALS["MANDRIGO"]["CONFIG"]["ROOT_PATH"].LOG_SETUP.$keys[$i].".".XML_EXT,"w");
+					if(!$f){
+						return false;
+					}
+				}
+				else{
+					$f=@fopen($GLOBALS["MANDRIGO"]["CONFIG"]["ROOT_PATH"].LOG_SETUP.$keys[$i].".".XML_EXT,"w");
+					if(!$f){
+						$GLOBALS["MANDRIGO"]["ERROR_LOGGER"]->el_adderror(2001,"display");
+						return false;
+					}
+				}
+				
+				$xml_doc=
+				"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
+				<error_log>		
+				";
+		
+				$som=count($this->document);
+				for($m=0;$m<$som;$m++){
+				 	$tmp=$this->document[$m];
+				 	$tmp["VALUE"][0]["data"]=ereg_replace(">","&gt;",$tmp["VALUE"][0]["data"]);
+				 	$tmp["VALUE"][0]["data"]=ereg_replace("<","&lt;",$tmp["VALUE"][0]["data"]);
+					$xml_doc.="
+						<msg>
+							<id>{$tmp["ID"][0]["data"]}</id>
+							<value>
+								{$tmp["VALUE"][0]["data"]}
+							</value>
+						</msg>
+					";
+				}
+		
+				$xml_doc.="</error_log>";
+				fwrite($f,$xml_doc);
+				fclose($f);
+			}
+		}
+	}
+	function pa_editlogconf($num,$add=false,$error=""){
+	 	$string="";	
+
+		$tmp=$this->document;
+		if($add){
+			$sotemp=count($tmp);
+			$new_array=array();
+			$part=true;
+			for($k=0;$k<$sotemp;$k++){
+				if((int)$tmp[$k+1]["ID"][0]["data"]>(int)$num&&$part){
+				 	$new_array[$k]=$tmp[$k];
+					$new_array[$k+1]["ID"][0]["data"]=$num;
+					$new_array[$k+1]["VALUE"][0]["data"]=$error;
+					$part=false;
+				}
+				else if($part){
+					$new_array[$k]=$tmp[$k];
+					if($k+1>=$sotemp){
+						$new_array[$k+1]["ID"][0]["data"]=$num;
+						$new_array[$k+1]["VALUE"][0]["data"]=$error;						
+					}
+				}
+				else{
+					$new_array[$k+1]=$tmp[$k];	
+				}
+			}
+			$this->document=$new_array;		
+		}
+		else{
+			$sotemp=count($tmp);
+			$new_array=array();
+			$part=true;
+			for($k=0;$k<$sotemp;$k++){
+				if($tmp[$k]["ID"][0]["data"]==(string)$num){
+					$part=false;
+				}
+				else if($part){
+					$new_array[$k]=$tmp[$k];
+				}
+				else{
+					$new_array[$k-1]=$tmp[$k];	
+				}
+			}
+			$this->document=$new_array;
+		}		
+	}
+
+    //
+    //private function el_xmlparse($path)
+    //
+    //Loads and parses an xml file
+    //
+    //INPUTS:
+    //$path	-	path to the xml file (default: )   
+    //
+    //returns true on success or false on fail
+	function pa_xmlparse($path){
+		$parser=xml_parser_create();
+	 	$this->document = array();
+		$this->currTag =& $this->document;
+		$this->tagStack = array();
+	    xml_set_object($parser, $this);
+	    xml_set_character_data_handler($parser, 'pa_datahandler');
+	    xml_set_element_handler($parser, 'pa_starthandler', 'pa_endhandler');
+	   	if(!($fp = @fopen($path, "r"))){
+	           return false;
+	    }
+		while($data = fread($fp, 4096)){
+	    	if(!xml_parse($parser, $data, feof($fp))){
+	        	return false;
+			}
+		}
+	    fclose($fp);
+	   	xml_parser_free($parser);
+	   	return true;
+	}
+	
+    //
+    //private function el_starthandler($parser, $name, $attribs)
+    //
+    //Required funtion to parse the beginning of each xml tag
+    //
+    //INPUTS:
+    //$parser	-	xml parser(default: )  
+	//$name		-	xml tag name (default: ) 
+    //$attribs	-	xml tag attributes (default: )
+	function pa_starthandler($parser, $name, $attribs){
+	    if(!isset($this->currTag[$name])){
+			$this->currTag[$name] = array();
+		}
+	      
+	    $newTag = array();
+	    if(!empty($attribs)){
+			$newTag['attr'] = $attribs;	
+		}
+	    array_push($this->currTag[$name], $newTag);
+	      
+		$t =& $this->currTag[$name];
+		$this->currTag =& $t[count($t)-1];
+		array_push($this->tagStack, $name);
+	}
+
+    //
+    //private function el_datahandler($parser, $data)
+    //
+    //Required funtion to parse the middle of each xml tag
+    //
+    //INPUTS:
+    //$parser	-	xml parser(default: )  
+	//$data		-	xml tag data (default: ) 	  
+	function pa_datahandler($parser, $data){
+	    $data = trim($data); 
+	    if(!empty($data)){
+	        if(isset($this->currTag['data'])){
+				$this->currTag['data'] .= $data;	
+			}
+	        else{
+				$this->currTag['data'] = $data;
+			}
+	    }
+	}
+	
+    //
+    //private function el_endhandler($parser, $name)
+    //
+    //Required funtion to parse the end of each xml tag
+    //
+    //INPUTS:
+    //$parser	-	xml parser(default: )  
+	//$name		-	xml tag name (default: )  
+	function pa_endhandler($parser, $name){
+	    $this->currTag =& $this->document;
+	    array_pop($this->tagStack);
+	      
+	    for($i = 0; $i < count($this->tagStack); $i++){
+	        $t =& $this->currTag[$this->tagStack[$i]];
+	        $this->currTag =& $t[count($t)-1];
+	    }
+	}
+}
