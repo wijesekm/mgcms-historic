a2e4c1c25df11535cf7726f0eb0b4fb969f0f83c
kwijesekera <kwijesekera@ddaff062-d30e-0410-924e-b8927a9d2c55>
2007-07-20 13:49:06 +0000

    Added filter class.  Will be used to filter out unwanted content like in mg_code blocks.

---
+++ trunk/mandrigo/inc/filter.class.php
@@ -0,0 +1,102 @@
+<?php
+/**********************************************************
+    filter.class.php
+	Last Edited By: Kevin Wijesekera
+	Date Last Edited: 07/20/07
+
+	Copyright (C) 2006-2007 the MandrigoCMS Group
+
+    ##########################################################
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+	###########################################################
+
+**********************************************************/
+
+//
+//To prevent direct script access
+//
+if(!defined("START_MANDRIGO")){
+    die($GLOBALS["MANDRIGO"]["CONFIG"]["DIE_STRING"]);
+}
+
+define("FILTER_PATH","/filters/");
+
+class filter{
+	
+	var $cur_filter;
+	var $str;
+	var $filter_size;
+	
+	function filter($name){
+		$this->change_filter($name);
+	}
+	function fi_change($name){
+	 	$file=$GLOBALS["MANDRIGO"]["CONFIG"]["ROOT_PATH"].$name.".filter.".PHP_EXT;
+		if(file_exists($file)&&!is_dir($file)){
+			if($GLOBALS["MANDRIGO"]["CONFIG"]["DEBUG_MODE"]){
+				if(!include_once($file)){
+					return false;
+				}
+			}
+			else{
+				if(!(@include_once($file))){
+					return false;
+				}
+			}
+		}
+		else{
+			return false;
+		}
+		$this->cur_filter=$filter;
+		$filter=array();
+		$this->filter_size=count(this->cur_filter);
+		return true;		
+	}
+	function fi_settext($stri){
+		$this->str=$stri;
+	}
+	function fi_returntext(){
+		return $this->str;
+	}
+	function fi_filter($mode,$censor=""){
+	 	$found_something=false;
+		for($i=0;$i<$this->filter_size;$i++){
+			switch($mode){
+				case 2:
+					if($this->fi_search($this->cur_filter[$i])){
+						$found_something=true;
+					}
+				break;
+				case 1:
+					if($this->fi_search($this->cur_filter[$i])){
+						$found_something=true;
+						$this->fi_destroy($this->cur_filter[$i]);
+					}				
+				break;
+				default:
+					return false;
+				break;
+			};
+		}
+		return $found_something
+	}
+	function fi_search($item){
+		return @mb_ereg($item,$this->str);
+	}
+	function fi_destroy($item,$censor=""){
+		return @mg_ereg_replace($item,$censor,$this->str);
+	}
+}
