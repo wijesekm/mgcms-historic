48426fc3a558fec384548dcefda0b0ac805e6671
kwijesekera <kwijesekera@ddaff062-d30e-0410-924e-b8927a9d2c55>
2007-07-24 17:01:35 +0000

    Added cache constants.

--- trunk/mandrigo/inc/ini/constants.ini.php
+++ trunk/mandrigo/inc/ini/constants.ini.php
@@ -48,6 +48,7 @@ define("TABLE_STATS_HITS","stats_hits");
 define("TABLE_STATS_IPS","stats_ips");
 define("TABLE_STATS_USERAGENTS","stats_uagents");
 define("TABLE_CLASSES","classes");
+define("TABLE_CACHE","cache")
 define("TABLE_CAPTCHA","captcha");
 define("TABLE_CAPTCHA_DATA","captchad");
 define("TABLE_ENVELOPE_DATA","envelopedata");
@@ -92,3 +93,4 @@ define("MULTIPART_ALT","multipart/alternative");
 define("TEXT_PLAIN","text/plain");
 define("TEXT_HTML","text/html");
 define("MULTI_ALT","multipart/alternative");
+define("CACHE_EXT",".cache");
