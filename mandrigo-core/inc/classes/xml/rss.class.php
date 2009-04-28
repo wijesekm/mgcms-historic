1056037d6cf650685bf2db38ab2ae5e5ee37c781
wijesekm <wijesekm@users.noreply.github.com>
2009-04-28 14:37:14 +0000

    1) Updated to be compatible with atom class and some bug fixes

--- mandrigo-core/inc/classes/xml/rss.class.php
+++ mandrigo-core/inc/classes/xml/rss.class.php
@@ -30,7 +30,6 @@ class rss extends mgXML{
 	
 	private $type;
 	private $count;
-	private $seq_count;
 	
 	public function __construct($pkg){
 		parent::__construct($pkg);
@@ -38,7 +37,7 @@ class rss extends mgXML{
 		$this->count=0;
 	}
 	
-	public function rss_setType($attrs,$type){
+	public function rss_start($attrs,$type){
 		parent::mxml_reset();
 		switch($type){
 			case '2.0':
@@ -50,54 +49,93 @@ class rss extends mgXML{
 			default:
 				$attrs['xmlns:rdf']='http://www.w3.org/1999/02/22-rdf-syntax-ns#';
 				$attrs['xmlns']='http://purl.org/rss/1.0/';
+				$attrs['xmlns:dc']='http://purl.org/dc/elements/1.1/';
 				parent::mxml_addTag('rdf:RDF',$attrs,false,false);
 				$this->type='1.0';
 			break;
 		}
 	}
 	
-	public function rss_setChannel($data){
+	public function rss_setPage($title,$page_url,$desc,$ts_ud,$other=false){
 		switch($this->type){
 			case '2.0':
 				parent::mxml_addTag('channel',false,false,array(0));
-				foreach($data as $key=>$value){
-					parent::mxml_addTag($key,false,$value,array(0,0));
+				parent::mxml_addTag('title',false,$title,array(0,0));
+				parent::mxml_addTag('link',false,$page_url,array(0,0));
+				parent::mxml_addTag('description',false,$desc,array(0,0));
+				parent::mxml_addTag('language',false,$GLOBALS['MG']['LANG']['NAME'],array(0,0));
+				parent::mxml_addTag('lastBuildDate',false,date('D, d M Y H:i:s T',$ts_ud),array(0,0));
+				$this->count=5;
+				if($other['img']){
+					parent::mxml_addTag('image',false,false,array(0,0));
+					$this->rss_addOther($other['img'],array(0,0,$c));
+					$other['img']=false;
 					$this->count++;
 				}
+				if($other['textInput']){
+					parent::mxml_addTag('textInput',false,false,array(0,0));
+					$this->rss_addOther($other['textInput'],array(0,0,$c));
+					$other['textInput']=false;
+					$this->count++;					
+				}
+				$this->rss_addOther($other,array(0,0));
 			break;
 			case '1.0':
 			default:
-				$this->seq_count=0;
 				$this->count++;
-				parent::mxml_addTag('channel',array('rdf:about'=>$data['link']),false,array(0));
-				foreach($data as $key=>$value){
-					parent::mxml_addTag($key,false,$value,array(0,0));
-					$this->seq_count++;
-				}
+				parent::mxml_addTag('channel',array('rdf:about'=>$page_url),false,array(0));
+				parent::mxml_addTag('title',false,$title,array(0,0));
+				parent::mxml_addTag('link',false,$page_url,array(0,0));
+				parent::mxml_addTag('description',false,$desc,array(0,0));
+				parent::mxml_addTag('dc:language',false,$GLOBALS['MG']['LANG']['NAME'],array(0,0));
+				parent::mxml_addtag('dc:date',false,date('c',$ts_ud),array(0,0));
 				parent::mxml_addtag('items',false,false,array(0,0));
-				parent::mxml_addtag('rdf:Seq',false,false,array(0,0,$this->seq_count));
+				parent::mxml_addtag('rdf:Seq',false,false,array(0,0,5));
+				$this->rss_addOther($other,array(0,0));
 			break;			
 		}
 	}
 	
-	public function rss_addPost($data){
+	public function rss_addPost($title,$link,$ts_ud,$summary,$other=false){
 		switch($this->type){
 			case '2.0':
 				parent::mxml_addtag('item',false,false,array(0,0));
-				foreach($data as $key=>$value){
-					parent::mxml_addTag($key,false,$value,array(0,0,$this->count));
-				}
+				parent::mxml_addTag('title',false,$title,array(0,0,$this->count));
+				parent::mxml_addTag('link',false,$link,array(0,0,$this->count));
+				parent::mxml_addTag('guid',false,$link,array(0,0,$this->count));
+				parent::mxml_addTag('pubDate',false,date('D, d M Y H:i:s T',$ts_ud),array(0,0,$this->count));
+				parent::mxml_addTag('description',fales,$summary,array(0,0,$this->count));
+				$this->rss_addOther($other,array(0,0,$this->count));
 				$this->count++;				
 			break;
 			case '1.0':
 			default:
-				parent::mxml_addtag('item',array('rdf:about'=>$data['link']),false,array(0));
-				parent::mxml_addtag('rdf:li',array('rdf:resource'=>$data['link']),false,array(0,0,$this->seq_count,0));
-				foreach($data as $key=>$value){
-					parent::mxml_addTag($key,false,$value,array(0,$this->count));
-				}
+				parent::mxml_addTag('item',array('rdf:about'=>$link),false,array(0));
+				parent::mxml_addTag('rdf:li',array('rdf:resource'=>$link),false,array(0,0,5,0));
+				parent::mxml_addTag('title',false,$title,array(0,$this->count));
+				parent::mxml_addTag('link',false,$link,array(0,$this->count));
+				parent::mxml_addTag('description',false,$summary,array(0,$this->count));
+				parent::mxml_addTag('dc:date',false,date('c',$ts_ud),array(0,$this->count));
+				$this->rss_addOther($other,array(0,$this->count));
 				$this->count++;
 			break;			
 		}
 	}
+
+	private function rss_addOther($other,$parents){
+		if(is_array($other)){
+			foreach($other as $key=>$val){
+				if(is_array($value)){
+					parent::mxml_addTag($key,$val[0],$val[1],$parents);
+				}
+				else if($value){
+					parent::mxml_addTag($key,false,$val,$parents);
+				}
+				if($this->type='2.0'){
+					$this->count++;
+				}
+			}
+		}
+	}
+
 }
