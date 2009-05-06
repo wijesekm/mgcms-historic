351173659221fab75bf50997d5a4b15d9bb6160d
wijesekm <wijesekm@ddaff062-d30e-0410-924e-b8927a9d2c55>
2009-05-06 15:06:41 +0000

    1) Added csv.class.php

---
+++ trunk/mandrigo-core/inc/classes/file/csv.class.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file                csv.class.php
+ * @author              Kevin Wijesekera
+ * @copyright   		2009
+ * @edited              5-06-2009
+ 
+ ###################################
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see http://www.gnu.org/licenses/.
+ ###################################
+ */
+
+if(!defined('STARTED')){
+	die();
+}
+
+class csv{
+	
+	private $csv_array=array();
+	
+	function csv_addLine($values){
+		$this->csv_array[]=$values;
+	}
+	
+	function csv_export($file=false,$delimiter=',',$enclosure='"'){
+		$delimiter_esc = preg_quote($delimiter, '/');
+    	$enclosure_esc = preg_quote($enclosure, '/'); 
+		$csv='';
+		foreach($this->csv_array as $subArray){
+			if(preg_match("/(?:${delimiter_esc}|${enclosure_esc}|\s)/", $subArray)){
+				$subArray=$enclosure . str_replace($enclosure, $enclosure . $enclosure, $subArray) . $enclosure;
+			}
+			$csv.=implode($delimiter,$subArray)."\n";
+		}
+		if($file){
+			if(!$f=fopen($file,'w')){
+				trigger_error('(CSV): Could not open csv file for writing: '.$file,E_USER_ERROR);
+				return false;
+			}
+			fwrite($f,$csv);
+			fclose($f);
+			return true;
+		}
+		return $csv;
+	}	
+}
