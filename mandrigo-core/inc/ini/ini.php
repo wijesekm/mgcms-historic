ddf580aefbaac85ed41597c79e523ffb09122f2b
Kevin <web@wijesekera-home.net>
2017-02-05 15:32:57 -0500

    Bugfix

--- mandrigo-core/inc/ini/ini.php
+++ mandrigo-core/inc/ini/ini.php
@@ -5,18 +5,18 @@
  * @author 		Kevin Wijesekera
  * @copyright 	2012
  * @edited		10-9-2012
- 
+
  ###################################
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see http://www.gnu.org/licenses/.
  ###################################
@@ -57,7 +57,7 @@ if(!defined('CRON')){
 	$load[]=array('mgcache','class','/classes/');
 	$load[]=array('session','class','/classes/auth/');
 }
-            
+
 if(defined('AJAX')){
     $load[]=array('ajax','class','/classes/');
 }
@@ -164,7 +164,7 @@ if(!defined('CRON')){
 		trigger_error('(INI): Invalid account type or not account type set', E_USER_ERROR);
 		$GLOBALS['MG']['ERROR']['LOGGER']->el_checkFatal();
 	}
-	
+
 	if(isset($GLOBALS['MG']['POST']['USER_NAME'])&&preg_match('/;/',$GLOBALS['MG']['POST']['USER_NAME'])){
 			$GLOBALS['MG']['COOKIE']['USER_NAME']=explode(';',$GLOBALS['MG']['POST']['USER_NAME']);
 			$GLOBALS['MG']['COOKIE']['USER_SESSION']=$GLOBALS['MG']['POST']['USER_SESSION'];
@@ -189,9 +189,9 @@ if(!defined('CRON')){
 	/**
 	 * Impersonate Settings
 	 */
-	 
+
 	 $GLOBALS['MG']['REAL_USER']=array();
-	 
+
 	if(isset($GLOBALS['MG']['SITE']['ALLOW_IMPERSONATE']) && isset($GLOBALS['MG']['COOKIE']['ALTERNATE_UID'])){
 	    if($GLOBALS['MG']['SITE']['ALLOW_IMPERSONATE'] == '1' && $GLOBALS['MG']['COOKIE']['ALTERNATE_UID'] != '' && mg_checkACL('*','admin')){
 	        if($act->act_isAccount($GLOBALS['MG']['COOKIE']['ALTERNATE_UID'])){
@@ -257,11 +257,11 @@ if(!defined('CRON')){
 					break;
 			}
 		}
-		$tmp=false;	
+		$tmp=false;
 	}
 	$tmp=false;
 	$GLOBALS['MG']['PAGE']['REDIRECT']=false;
-	
+
 	if(mg_checkACL($GLOBALS['MG']['PAGE']['PATH'],'admin')&&isset($GLOBALS['MG']['GET']['USER_NAME'])&&$GLOBALS['MG']['GET']['USER_NAME']!=''){
 		$GLOBALS['MG']['USER']=$act->act_load($GLOBALS['MG']['GET']['USER_NAME']);
 		$GLOBALS['MG']['USER']=$GLOBALS['MG']['USER'][$GLOBALS['MG']['GET']['USER_NAME']];
@@ -290,8 +290,8 @@ else{
 					break;
 				};
 			}
-			
-			
+
+
 		}
 		$tmp=false;
 	}
@@ -304,7 +304,7 @@ $act=false;
 $lang=false;
 if($GLOBALS['MG']['SITE']['LANG_ALLOW_OVERRIDE']=='1'){
 	if(!empty($GLOBALS['MG']['COOKIE']['LANGUAGE'])){
-		$lang=$GLOBALS['MG']['COOKIE']['LANGUAGE'];	
+		$lang=$GLOBALS['MG']['COOKIE']['LANGUAGE'];
 	}
 	else if(!empty($GLOBALS['MG']['GET']['LANGUAGE'])){
 		$lang=$GLOBALS['MG']['GET']['LANGUAGE'];
@@ -316,7 +316,7 @@ if($GLOBALS['MG']['SITE']['LANG_ALLOW_OVERRIDE']=='1'){
 		if(!empty($GLOBALS['MG']['SITE']['MOBILE_SUPPORT'])){
 			if(mginit_mobileDeviceCheck()){
 				$lang.=$GLOBALS['MG']['SITE']['MOBILE_SUPPORT'];
-			}			
+			}
 		}
 	}
 
@@ -326,13 +326,13 @@ if(!$lang){
 	if(!empty($GLOBALS['MG']['SITE']['MOBILE_SUPPORT'])){
 		if(mginit_mobileDeviceCheck()){
 			$lang.=$GLOBALS['MG']['SITE']['MOBILE_SUPPORT'];
-		}			
+		}
 	}
 }
 $lang=$GLOBALS['MG']['SQL']->sql_fetcharray(array(TABLE_PREFIX.'langsets'),false,array(array(false,false,'lang_name','=',strtolower($lang))));
 
 if(!$lang[0]['lang_id']){
-	$lang=$GLOBALS['MG']['SQL']->sql_fetcharray(array(TABLE_PREFIX.'langsets'),false,array(array(false,false,'lang_name','=',strtolower($GLOBALS['MG']['SITE']['DEFAULT_LANGUAGE']))));	
+	$lang=$GLOBALS['MG']['SQL']->sql_fetcharray(array(TABLE_PREFIX.'langsets'),false,array(array(false,false,'lang_name','=',strtolower($GLOBALS['MG']['SITE']['DEFAULT_LANGUAGE']))));
 }
 
 mginit_loadLang($lang[0]['lang_name']);
@@ -403,9 +403,9 @@ function mginit_setHeader($name,$value){
 }
 
 function mginit_mobileDeviceCheck(){
-	$useragent=$_SERVER['HTTP_USER_AGENT'];
+	$useragent=isset($_SERVER['HTTP_USER_AGENT'])?$_SERVER['HTTP_USER_AGENT']:'';
 	if(preg_match('/android|avantgo|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i',substr($useragent,0,4))){
 		return true;
 	}
-	return false;	
+	return false;
 }
